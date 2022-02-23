32d80f53110eaca69968681eb62486719730d549
pfelto <paul.r.felton@gmail.com>
2022-02-23 06:06:34 -0500

    Tweaks to palindrome algorithm as it did not need to be so complex

--- palindrome.js
+++ palindrome.js
@@ -1,17 +1,9 @@
 function palindrome(str) {
-  const x = str.trim().replace(/\W|_/g, '').toLowerCase().split('');
-  let midpoint;
-  if (x.length % 2 === 0) {
-    midpoint = x.length / 2;
-  } else {
-    midpoint = x.length / 2 + 1;
-  }
-  const y = x.slice(0, x.length / 2);
-  const z = x.slice(midpoint).reverse();
-  for (let i = 0; i < y.length; i += 1) {
-    if (y[i] !== z[i]) return false;
-  }
-  return true;
+  if (typeof str !== 'string') return false;
+  const x = str.trim().replace(/\W|_/g, '').toLowerCase().split('')
+    .reverse()
+    .join('');
+  return str.trim().replace(/\W|_/g, '').toLowerCase() === x;
 }
 
 palindrome('   _e y  e ');
